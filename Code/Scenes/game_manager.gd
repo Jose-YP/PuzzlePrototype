d7e16cf3842201691a072e101002b0062805234e
Jose-YP <joseaw2003@gmail.com>
2024-05-09 00:09:56 -0400

    Move Next Containers to left

--- Code/Scenes/game_manager.gd
+++ Code/Scenes/game_manager.gd
@@ -1,11 +1,11 @@
 extends CanvasLayer
 
+@onready var nextBeads = $VBoxContainer/NextBeads
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func update_next(beads):
+	for i in range(3):
+		nextBeads.update(i,beads[i])
