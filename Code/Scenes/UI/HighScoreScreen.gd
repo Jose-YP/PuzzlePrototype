9d1b47574cfc28f14c0de362afd27b27cbd5e24f
Jose-YP <joseaw2003@gmail.com>
2024-05-10 20:08:38 -0400

    Reverting to ui_buttons for inputs

--- Code/Scenes/UI/HighScoreScreen.gd
+++ Code/Scenes/UI/HighScoreScreen.gd
@@ -10,19 +10,30 @@ const characters: Array[String] = ["A","B","C","D","E","F","G","H","I","J","K","
 "2","3","4","5","6","7","8","9",".","?","!"]
 
 var score: int = 0
-var placement: int = 0
+var placement: int = 6
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$VBoxContainer/RichTextLabel.clear()
 	$VBoxContainer/RichTextLabel.append_text(str("[center] Reached a High Score of ",score))
 	$VBoxContainer/RichTextLabel2.clear()
-	$VBoxContainer/RichTextLabel2.append_text(str("[center] Placed in ",score))
+	$VBoxContainer/RichTextLabel2.append_text(str("[center] Placed in ",convert_placement(placement)," place!"))
 	var nameIndexes = charIndex(username)
 	print(nameIndexes)
 	
 	for i in range(charInputs.size()):
 		charInputs[i].text = characters[nameIndexes[i]]
+
+func convert_placement(place):
+	match place:
+		1:
+			return str(place,"st")
+		2:
+			return str(place,"nd")
+		3:
+			return str(place,"rd")
+		_:
+			return str(place,"th")
 
 func charIndex(input) -> Array[int]:
 	var indexes: Array[int] = []
