c511c465e6d8bf8667c473fbba8a431afff73fb4
Jose-YP <joseaw2003@gmail.com>
2024-05-14 15:23:40 -0400

    Making How to Play Screen
    Doing what I should've long ago and adding the rules to the globals

--- Code/Constants/how_2_play.gd
+++ Code/Constants/how_2_play.gd
@@ -11,14 +11,19 @@ signal boardSFX(index)
 signal makeSFX(index)
 signal exit
 
+var breakNum: int = 1
+
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	pass
-
+	if Input.is_action_just_pressed("Break") and breakNum > 0:
+		pass
+	if Input.is_action_just_pressed("ui_accept"):
+		%Next.press()
+	if Input.is_action_just_pressed("ui_cancel"):
+		%Exit.press()
 
 func _on_exit_pressed():
 	makeSFX.emit(2)
@@ -31,4 +36,5 @@ func _on_next_pressed():
 	else:
 		infoTabs.current_tab -= 1
 		%Next.text = str("Controls")
+	makeSFX.emit(1)
 	
