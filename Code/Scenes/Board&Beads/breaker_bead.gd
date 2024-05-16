97a86adca49e342fe19b0b2616f9b81039a9f2d4
Jose-YP <joseaw2003@gmail.com>
2024-05-16 11:23:30 -0400

    Added SFX for ripple + starting to make chain feedback

--- Code/Scenes/Board&Beads/breaker_bead.gd
+++ Code/Scenes/Board&Beads/breaker_bead.gd
@@ -1,1 +1,7 @@
 extends Node2D
+
+signal find_adjacent
+
+var currentType: String = "Breaker"
+var adjacent: Array = []
+var breaking: bool = false
--- Code/Scenes/UI/right_ui.gd
+++ Code/Scenes/UI/right_ui.gd
@@ -1,5 +1,7 @@
 extends Control
 
+@export var chainDisplayTiming: float = 1.0
+
 signal levelUp(level)
 signal HighScore
 
@@ -8,11 +10,16 @@ var regScore: int = 0
 var HiScore: int = 0
 var level: int = 1
 
-# Called when the node enters the scene tree for the first time.
+#______________________________
+#INITIALIZATION
+#______________________________
 func _ready() -> void:
 	HiScore = Globals.get_extreme()[0]
 	%HiScoreText.text = str("HISCORE: ", HiScore)
 
+#______________________________
+#SCORE MANIP
+#______________________________
 func update_score(score) -> void:
 	regScore = score
 	%ScoreText.text = str("SCORE: ",regScore)
@@ -24,6 +31,9 @@ func update_HiScore(score) -> void:
 	%HiScoreText.text = str("HISCORE: ", score)
 	HighScore.emit()
 
+#______________________________
+#BEAD & LEVEL MANIP
+#______________________________
 func update_beads(beads) -> void:
 	%BeadText.text = str("BEADS: ", beads)
 	var levelThreshold = level_upThreshold(level)
@@ -35,7 +45,7 @@ func update_beads(beads) -> void:
 		#Check for even higher levels
 		update_beads(beads)
 
-func level_upThreshold(lv):
+func level_upThreshold(lv) -> int:
 	if lv <= 1:
 		return roundi(rules.bead_levelUp + lv * rules.levelUp_rate)
 	else:
@@ -46,3 +56,26 @@ func update_level() -> void:
 	%LevelText.text = str("LEVEL: ", level)
 	Globals.level = level
 	levelUp.emit(level)
+
+#______________________________
+#CHAIN DISPLAY MANIP
+#______________________________
+func show_display() -> void:
+	var displayTween = get_tree().create_tween()
+	displayTween.tween_property($VBoxContainer/ChainTotals, "modulate", Color.WHITE, chainDisplayTiming)
+	$VBoxContainer/ChainTotals/RichTextLabel.clear()
+
+func update_display(beads, links, chains) -> void:
+	$VBoxContainer/ChainTotals/RichTextLabel.clear()
+	var chainText: String = "\n"
+	if chains > 1:
+		chainText = str(chainText, chains, " Chains")
+	else:
+		chainText = str(chainText, chains, " Chain")
+	
+	$VBoxContainer/ChainTotals/RichTextLabel.append_text(str(beads," Beads\n",
+	links," Links",chains))
+
+func remove_display() -> void:
+	var displayTween = get_tree().create_tween()
+	displayTween.tween_property($VBoxContainer/ChainTotals, "modulate", Color.TRANSPARENT, chainDisplayTiming)
