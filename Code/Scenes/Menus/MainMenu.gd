c8934b1a975b9415c651d637b03cc928d021170b
Jose-YP <joseaw2003@gmail.com>
2024-05-29 22:29:20 -0400

    Web build has more errors that I thought

--- Code/Scenes/Menus/MainMenu.gd
+++ Code/Scenes/Menus/MainMenu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 @export var newH2pPosition: Vector2 = Vector2(-526,-330)
-@export var orgH2pPosition: Vector2 = Vector2(-526,-1000)
+@export var orgH2pPosition: Vector2 = Vector2(-526,-1500)
 
 signal switchPlay
 signal switchOptions
