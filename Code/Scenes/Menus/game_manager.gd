1e0f42bc8bf2fc42316593824c75b5ef20665897
Jose-YP <joseaw2003@gmail.com>
2024-06-06 19:06:19 -0400

    First song Fades in and out

--- Code/Scenes/Menus/game_manager.gd
+++ Code/Scenes/Menus/game_manager.gd
@@ -5,11 +5,12 @@ extends CanvasLayer
 
 @onready var BoardSFX: Array[AudioStreamPlayer] = [%HoriMove, %Rotate, 
 %HardDrop, %SoftDrop, %Twinkle, %Zap, %LevelUp, %ETC, %AllFall]
-@onready var BreakSFX: Array[AudioStreamPlayer] = [%Roar, %Roar2, %Roar3]
-@onready var MenuSFX: Array[AudioStreamPlayer] = [%MenuMove, %MenuConfirm,
- %MenuDeselect, %Pause]
-@onready var music: AudioStreamPlayer = $Music/Music
+@onready var BreakSFX: Array[Node] = $BoardSFX/BreakSFX.get_children()
+@onready var MenuSFX: Array[Node] = $BoardMusic.get_children()
+@onready var ETCMusic: Array[AudioStreamPlayer] = [$ETCMusic/MainMenu,$ETCMusic/EndRun]
+@onready var BoardMusic: Array[Node] = $BoardMusic.get_children()
 @onready var currentScene = $MainMenu
+@onready var editableMusicBus = AudioServer.get_bus_index("ManageMusic")
 
 #Scenes for loading
 const boardScene: String = "res://Scenes/Board&Beads/Board.tscn"
@@ -19,15 +20,19 @@ const mainMenuScene = preload("res://Scenes/MainMenu/MainMenu.tscn")
 const loadingScreen = preload("res://Scenes/Constants/ETC/load_screen.tscn")
 
 var unpausing: bool = false
+var currentSong: AudioStreamPlayer
+var loopVal: float = 0.0
+var loopedSong: bool = false
 
-func _ready():
+func _ready() -> void:
 	BoardSFX[0].play()
 	BoardSFX[0].stop()
 	BoardSFX[1].play()
 	BoardSFX[1].stop()
+	ready_playing(ETCMusic[0])
 	FinalGlobal.finalReady(Newgrounds)
 
-func _process(_delta):
+func _process(_delta) -> void:
 	if Input.is_action_just_pressed("Pause") and not unpausing:
 		play_menu_sfx(3)
 		var currently = get_tree().paused
@@ -36,6 +41,13 @@ func _process(_delta):
 		$PauseScreen.play_sfx()
 	elif Input.is_action_just_pressed("Pause"):
 		unpausing = false
+	
+	if loopVal > currentSong.get_playback_position():
+		print("YOU LOOPED")
+		loopedSong = true
+		
+		
+	loopVal = currentSong.get_playback_position()
 
 #-----------------------------------------
 #SCENE SWITCHING
@@ -58,12 +70,11 @@ func loadScene(scene) -> void:
 	currentScene.next_scene = scene
 	match scene:
 		boardScene:
-			
 			currentScene.connect("finished", board_scene_loaded)
 		optionsScene:
 			currentScene.connect("finished", option_scene_loaded)
 
-func board_scene_loaded(scene):
+func board_scene_loaded(scene) -> void:
 	changeScene(scene)
 	currentScene.connect("playSFX",_on_board_play_sfx)
 	currentScene.connect("playBreak", _on_board_play_break_sfx)
@@ -72,58 +83,72 @@ func board_scene_loaded(scene):
 	$PauseScreen.entered_board(true)
 	
 
-func option_scene_loaded(scene):
+func option_scene_loaded(scene) -> void:
 	changeScene(scene)
 	currentScene.connect("main",back_to_menu)
 	currentScene.connect("makeNoise",_on_option_make_noise)
-	currentScene.connect("testMusic",_on_option_test_music)
 
-func back_to_menu():
+func back_to_menu() -> void:
 	changeScene(mainMenuScene)
 	currentScene.connect("switchOptions", _on_main_menu_switch_options)
 	currentScene.connect("switchPlay", _on_main_menu_switch_play)
 	currentScene.connect("boardSFX", _on_board_play_sfx)
 	currentScene.connect("playSFX", play_menu_sfx)
 
-func play_menu_sfx(index):
+func play_menu_sfx(index) -> void:
 	MenuSFX[index].play()
 
 #-----------------------------------------
 #MAIN MENU SIGNALS
 #-----------------------------------------
-func _on_main_menu_switch_options():
+func _on_main_menu_switch_options() -> void:
 	loadScene(optionsScene)
 
-func _on_main_menu_switch_play():
+func _on_main_menu_switch_play() -> void:
 	MenuSFX[1].play()
 	loadScene(boardScene)
 
 #-----------------------------------------
 #BOARD SIGNALS
 #-----------------------------------------
-func _on_board_play_sfx(index):
+func _on_board_play_sfx(index) -> void:
 	BoardSFX[index].play()
 
-func _on_board_play_break_sfx(index):
+func _on_board_play_break_sfx(index) -> void:
 	BreakSFX[index].play()
 
-func on_board_retry(onBoard = true):
+func on_board_retry(onBoard = true) -> void:
 	if onBoard:
 		loadScene(boardScene)
 
 #-----------------------------------------
 #OPTION MENU SIGNALS
 #-----------------------------------------
-func _on_option_make_noise():
+func _on_option_make_noise() -> void:
 	%ETC.play()
-
-func _on_option_test_music():
-	if music:
-		music.play()
 
 #-----------------------------------------
 #OTHER
 #-----------------------------------------
-func _on_pause_screen_play_sfx():
+func _on_pause_screen_play_sfx() -> void:
 	unpausing = true
 	MenuSFX[3].play()
+
+func ready_playing(song) -> void:
+	var fadeIn = create_tween().set_ease(Tween.EASE_IN)
+	fadeIn.tween_method(fade_music, 0.0, 1.0, 10)
+	currentSong = song
+	song.play()
+
+func play_music(song) -> void:
+	if MusicOn:
+		var MusicBus = AudioServer.get_bus_index("ManageMusic")
+		var fadeOut = create_tween().set_ease(Tween.EASE_IN)
+		fadeOut.tween_method(MusicBus.set_bus_volume_db, linear_to_db(1.0), linear_to_db(0.0), .5)
+		currentSong.stop()
+		song.play()
+		currentSong = song
+		fadeOut.tween_method(MusicBus.set_bus_volume_db, linear_to_db(0.0), linear_to_db(1.0), .5)
+
+func fade_music(value) -> void:
+	AudioServer.set_bus_volume_db(editableMusicBus, linear_to_db(value))
