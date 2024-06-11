4d13a54b8b25043315459cb9b3589fd1e28cbf19
Jose-YP <joseaw2003@gmail.com>
2024-06-11 15:34:07 -0400

    Animations work thankfully

--- Code/Constants/test.gd
+++ Code/Constants/test.gd
@@ -1,11 +1,19 @@
 extends Control
 
+@onready var buttons = [$TextureButton, $TextureButton2, $TextureButton3]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$TextureButton.grab_focus()
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _process(_delta):
+	if (Input.is_action_just_pressed("ui_left") or Input.is_action_just_pressed("ui_right")
+	 or Input.is_action_just_pressed("ui_down") or Input.is_action_just_pressed("ui_up")):
+		print("Move")
+		for button in buttons: 
+			print(button, button.texture_normal, button.animations.get_assigned_animation())
+			
+		print(get_viewport().gui_get_focus_owner())
+		print()
+		
--- Code/Scenes/Menus/MainMenuButton.gd
+++ Code/Scenes/Menus/MainMenuButton.gd
@@ -6,19 +6,26 @@ extends TextureButton
 
 signal finish
 
+func _ready():
+	texture_normal = texture_normal.duplicate()
+
+func _process(delta):
+	print(animations.get_assigned_animation())
+
 func _on_pressed():
-	print("B")
-	animations.play("Pressed")
-	await finish
-	print("C")
-	animations.play("Pressed",true)
+	animations.play("MainMenuButtons/Pressed")
+	texture_normal = texture_normal.duplicate()
 
 func _on_focus_entered():
-	print(self)
-	animations.play("Hover", -1.0, 1.0)
+	animations.play("MainMenuButtons/Hover")
+	texture_normal = texture_normal.duplicate()
 
 func _on_focus_exited():
-	animations.play("RESET", -1.0, 2.0)
+	animations.stop()
+	animations.play("MainMenuButtons/Off")
+	animations.play("RESET")
+	texture_normal = texture_normal.duplicate()
+	print("RESERT", self, texture_normal)
 
 func finish_signal():
 	finish.emit()
