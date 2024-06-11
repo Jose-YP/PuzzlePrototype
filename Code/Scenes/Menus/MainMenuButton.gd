5706c800b9ce4f06db89e397eee3d98cc31f9069
Jose-YP <joseaw2003@gmail.com>
2024-06-10 20:12:38 -0400

    New Beads dropped

--- Code/Scenes/Menus/MainMenuButton.gd
+++ Code/Scenes/Menus/MainMenuButton.gd
@@ -4,8 +4,24 @@ extends TextureButton
 
 @onready var animations: AnimationPlayer = $AnimationPlayer
 
+signal finish
+
 func _ready():
-	pass
+	$".".grab_focus()
 
 func _on_pressed():
-	animations.play("Pressed", -1.0, 2.0)
+	print("B")
+	animations.play("Pressed")
+	await finish
+	print("C")
+	animations.play("Pressed",true)
+
+func _on_focus_entered():
+	print("a")
+	animations.play("Hover", -1.0, 1.0)
+
+func _on_focus_exited():
+	animations.play("RESET", -1.0, 2.0)
+
+func finish_signal():
+	finish.emit()
