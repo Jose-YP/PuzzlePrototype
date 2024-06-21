e96ebd11adc873b67c265b324b9e30bd753571a4
Jose-YP <joseaw2003@gmail.com>
2024-06-21 08:52:10 -0400

    New Break Meter

--- Code/Scenes/UI/BreakProgress.gd
+++ Code/Scenes/UI/BreakProgress.gd
@@ -1,14 +1,15 @@
 extends HBoxContainer
 
-@onready var progressBar: ColorRect = $PanelContainer/BreakProgress
-@onready var breakNotifier: PanelContainer = $PanelContainer2
-@onready var breakText: Label = $PanelContainer/Label
+@onready var progressBar: TextureProgressBar = $Progress/TextureProgressBar
+@onready var rippleCenter: ColorRect = $Progress/BreakProgress
+@onready var breakNotifier: PanelContainer = $Info
+@onready var breakText: Label = $Info/Label
 
 func _ready():
 	if Globals.rules.breakBead:
-		$PanelContainer2/VBoxContainer/RichTextLabel.text = "TO SPAWN A BREAKER BEAD"
+		$Info/VBoxContainer/RichTextLabel.text = "TO SPAWN A BREAKER BEAD"
 	
-	Globals.show_controls($PanelContainer2/VBoxContainer/RichTextLabel2/TextureRect)
+	Globals.show_controls($Info/VBoxContainer/RichTextLabel2/TextureRect)
 
 # Called when the node enters the scene tree for the first time.
 func set_ripple_center() -> Vector2:
