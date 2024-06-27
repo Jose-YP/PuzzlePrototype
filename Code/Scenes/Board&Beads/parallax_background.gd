1aa7424ce3bed43d317eaf89826be267c8c4e0df
Jose-YP <joseaw2003@gmail.com>
2024-06-27 11:31:12 -0400

    Need to get background to update proeprtly

--- Code/Scenes/Board&Beads/parallax_background.gd
+++ Code/Scenes/Board&Beads/parallax_background.gd
@@ -1,8 +1,21 @@
-@tool
 extends ParallaxBackground
 
-@export var scrolling_speed: float = 100.0
+@export var scrolling_speed: Vector2 = Vector2(100.0, 50.0)
+@export var first_color: Color = Color.WHITE
+@export var second_color: Color = Color.WHITE
+
+@onready var mirroring: Vector2 = $FirstLayer.motion_mirroring
+
+func _ready():
+	$FirstLayer.modulate = first_color
+	$SecondLayer.modulate = Color(second_color, .15)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	scroll_offset += scrolling_speed * delta
+	scroll_offset += (scrolling_speed * Vector2(delta, delta))
+	
+	#I wanna prevent the number from getting to big but
+	
+	#if (int(scroll_offset.x) % int(mirroring.x) == 0
+	#and (int(scroll_offset.y) % int(mirroring.y)) == 0):
+		#scroll_offset = Vector2.ZERO
