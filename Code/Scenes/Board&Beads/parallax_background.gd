8c2ef6d21f4abbc3018ecc4b2353f73b201acc53
Jose-YP <joseaw2003@gmail.com>
2024-06-28 21:13:22 -0400

    Bandage fix for BG

--- Code/Scenes/Board&Beads/parallax_background.gd
+++ Code/Scenes/Board&Beads/parallax_background.gd
@@ -1,5 +1,6 @@
 extends ParallaxBackground
 
+@export var debug_speed: float = 2
 @export var scrolling_speed: Vector2 = Vector2(-100.0, -50.0)
 @export var first_color: Color = Color.WHITE
 @export var second_color: Color = Color.WHITE
@@ -12,20 +13,20 @@ var colorGroups: Array[Array] = [DomainColors, EnergyColors]
 var tweening
 
 func _ready():
-	$FirstLayer.modulate = Color(first_color, .90)
+	$FirstLayer.modulate = Color(first_color)
 	$SecondLayer.modulate = Color(second_color, .35)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if not  tweening:
-		scroll_offset += (scrolling_speed * Vector2(delta, delta))
+		scroll_offset += (scrolling_speed * debug_speed * Vector2(delta, delta))
 	
 		#I wanna prevent the number from getting to big but
-		if (int(scroll_offset.x) % int(mirroring.x) == 0
-		or (int(scroll_offset.y) % int(mirroring.y)) == 0):
-			var reverseTween = create_tween().set_ease(Tween.EASE_IN)
-			reverseTween.tween_property(self, "scroll_offset", Vector2.ZERO, scroll_offset.x/50)
-			tweening = true
-			await reverseTween.finished
-			tweening = false
+		#if (int(scroll_offset.x) % int(mirroring.x) == 0
+		#or (int(scroll_offset.y) % int(mirroring.y)) == 0):
+			#var reverseTween = create_tween().set_ease(Tween.EASE_IN)
+			#reverseTween.tween_property(self, "scroll_offset", Vector2.ZERO, scroll_offset.x/50)
+			#tweening = true
+			#await reverseTween.finished
+			#tweening = false
 		
